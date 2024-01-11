5481f58c11b9f3373b5231d3854731e5b9586df4
SeniorTech <tech.eng.me@gmail.com>
2024-01-11 09:19:46 -0500

    I fix image quality

--- components/MenuList/MenuList.tsx
+++ components/MenuList/MenuList.tsx
@@ -44,7 +44,7 @@ const MenuList = () => {
           {authenticated && (
             <button type="button" className="ml-[24px]" onClick={() => router.push("/checkout")}>
               <Image
-                link="/images/cart-shopping-regular.png"
+                link="/images/cart-shopping-regular.svg"
                 blurLink="/images/cart-shopping-regular.png"
                 containerClasses="w-[24px] aspect-[24/21]"
                 alt="not found icon"
