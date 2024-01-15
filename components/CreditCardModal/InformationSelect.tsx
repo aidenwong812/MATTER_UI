a0a7153f01f0d5ae62498e6b30ed92c862536652
SeniorTech <tech.eng.me@gmail.com>
2024-01-15 15:03:11 -0500

    I add total price

--- components/CreditCardModal/InformationSelect.tsx
+++ components/CreditCardModal/InformationSelect.tsx
@@ -1,52 +1,61 @@
+import { formatEther } from "viem"
 import Image from "../../shared/Image"
 import CreditCardInformation from "./CreditCardInformation"
 import DeliveryInformation from "./DeliveryInfomation"
+import { useCheckOut } from "../../providers/CheckOutProvider"
+import useEthPrice from "../../hooks/useEthPrice"
 
-const InformationSelect = () => (
-  <div className="bg-white w-full py-[40px] flex flex-col items-center">
-    <Image
-      link="/images/matter.svg"
-      blurLink="/images/matter.svg"
-      containerClasses="w-[111px] aspect-[111/26]"
-      alt="not found icon"
-    />
-    <p className="mt-[30px]">Secure checkout powered by</p>
-    <Image
-      link="/images/stripe.svg"
-      blurLink="/images/stripe.png"
-      containerClasses="w-[63px] aspect-[63/30]"
-      alt="not found icon"
-    />
-    <DeliveryInformation />
-    <CreditCardInformation />
-    <div
-      className="flex justify-between items-center w-full
-              p-[24px] border-b border-b-gray_3"
-    >
-      <p
-        className="text-black font-[400] text-[16px] leading-[100%]
-                  tracking-[-0.4px]"
+const InformationSelect = () => {
+  const { getUsdConversion } = useEthPrice()
+  const { totalPrice } = useCheckOut()
+  const usdPrice = getUsdConversion(formatEther(totalPrice.toBigInt()))
+
+  return (
+    <div className="bg-white w-full py-[40px] flex flex-col items-center">
+      <Image
+        link="/images/matter.svg"
+        blurLink="/images/matter.svg"
+        containerClasses="w-[111px] aspect-[111/26]"
+        alt="not found icon"
+      />
+      <p className="mt-[30px]">Secure checkout powered by</p>
+      <Image
+        link="/images/stripe.svg"
+        blurLink="/images/stripe.png"
+        containerClasses="w-[63px] aspect-[63/30]"
+        alt="not found icon"
+      />
+      <DeliveryInformation />
+      <CreditCardInformation />
+      <div
+        className="flex justify-between items-center w-full
+                p-[24px] border-b border-b-gray_3"
       >
-        Order Subtotal
+        <p
+          className="text-black font-[400] text-[16px] leading-[100%]
+                    tracking-[-0.4px]"
+        >
+          Order Subtotal
+        </p>
+        <p
+          className="text-black font-[400] text-[16px] leading-[100%]
+                    tracking-[-0.4px]"
+        >
+          ${usdPrice}
+        </p>
+      </div>
+      <p className="text-[12px] text-gray_6 py-[24px]  mb-[32px]">
+        {`By tapping "Submit Payment", I agree to the Temrs of Sale.`}
       </p>
-      <p
-        className="text-black font-[400] text-[16px] leading-[100%]
-                  tracking-[-0.4px]"
+      <button
+        type="button"
+        className="border-none bg-gray_1 px-[51px] py-[16px] rounded-full
+                    font-[400] leading-[120%] text-[16px] w-[327px] text-gray_6"
       >
-        $10.00
-      </p>
+        Submit Payment
+      </button>
     </div>
-    <p className="text-[12px] text-gray_6 py-[24px]  mb-[32px]">
-      {`By tapping "Submit Payment", I agree to the Temrs of Sale.`}
-    </p>
-    <button
-      type="button"
-      className="border-none bg-gray_1 px-[51px] py-[16px] rounded-full
-                  font-[400] leading-[120%] text-[16px] w-[327px] text-gray_6"
-    >
-      Submit Payment
-    </button>
-  </div>
-)
+  )
+}
 
 export default InformationSelect
