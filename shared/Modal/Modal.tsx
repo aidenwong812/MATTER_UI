ebe4058adefc2b5a0fc9ca469f8a016b4c78b9db
SeniorTech <tech.eng.me@gmail.com>
2024-01-18 11:39:28 -0500

    I fix feedback

--- shared/Modal/Modal.tsx
+++ shared/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react"
 import FadeInWhenVisible from "../../components/FadeInWhenVisible"
+import { motion } from "framer-motion"
 
 interface IModal {
   onClose: () => any
@@ -10,12 +11,20 @@ interface IModal {
 
 function Modal({ children, isVisible, containerClassName, onClose }: IModal) {
   return (
-    <div
+    <motion.div
       className={`fixed top-0 left-0 w-screen h-screen 
         z-40 px-[20px] md:px-0
-        flex justify-center items-center pt-[56px]
-        ${isVisible ? "visible" : "hidden"}
+        justify-center items-center pt-[56px]
       `}
+      animate={{
+        display: isVisible ? "flex" : "none",
+      }}
+      initial={{
+        display: isVisible ? "none" : "flex",
+      }}
+      transition={{
+        delay: isVisible ? 0 : 0.5,
+      }}
     >
       <div
         className="absolute backdrop-blur-[5px] 
@@ -24,14 +33,25 @@ function Modal({ children, isVisible, containerClassName, onClose }: IModal) {
           e.target === e.currentTarget && onClose ? await onClose() : () => {}
         }
       />
-      <FadeInWhenVisible
+      <motion.div
         className={`relative z-[2] max-h-[90%]
-      overflow-y-auto shadow-gray_shadow rounded-[10px]
-      ${containerClassName || ""}`}
+        overflow-y-auto shadow-gray_shadow rounded-[10px]
+        ${containerClassName || ""}`}
+        animate={{
+          opacity: isVisible ? 1 : 0,
+          y: isVisible ? 0 : 100,
+        }}
+        initial={{
+          opacity: isVisible ? 0 : 1,
+          y: isVisible ? 100 : 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
       >
         {children}
-      </FadeInWhenVisible>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
