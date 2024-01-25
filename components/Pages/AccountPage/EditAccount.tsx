fd0ff9baf28dd9aadccd8ebaf813a2f53510e589
SeniorTech <tech.eng.me@gmail.com>
2024-01-25 10:18:37 -0500

    I fix background

--- components/Pages/AccountPage/EditAccount.tsx
+++ components/Pages/AccountPage/EditAccount.tsx
@@ -19,7 +19,7 @@ const EditAccount = () => {
   return (
     <div
       className="w-full md:w-[375px] shadow-gray_shadow rounded-[10px]
-        py-[40px] px-[26px] flex flex-col items-center"
+        py-[40px] px-[26px] flex flex-col items-center bg-white"
     >
       <div
         className="flex bg-gray_10 justify-center items-center
