7cb8dd3185dd7bb1bc11d1fbf57edb020e31c0f1
SeniorTech <tech.eng.me@gmail.com>
2024-01-25 10:36:51 -0500

    I removed unused code

--- lib/alchemy/getAlchemyRpcUrl.tsx
+++
@@ -1,6 +0,0 @@
-import getAlchemyBaseUrl from "./getAlchemyBaseUrl"
-
-const getAlchemyRpcUrl = (chainId) =>
-  `${getAlchemyBaseUrl(chainId)}v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
-
-export default getAlchemyRpcUrl
