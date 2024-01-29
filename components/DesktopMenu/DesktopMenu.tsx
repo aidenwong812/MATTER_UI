54a639df6fe9fc7adf3ea17d702d28e0a64a84f6
SeniorTech <tech.eng.me@gmail.com>
2024-01-29 11:19:39 -0500

    I fix feedbak

--- components/DesktopMenu/DesktopMenu.tsx
+++ components/DesktopMenu/DesktopMenu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Buttons from "../Header/Buttons"
 
 const DesktopMenu = () => (
@@ -5,9 +6,15 @@ const DesktopMenu = () => (
     className="flex gap-x-[32px] items-center justify-end
           flex-grow text-gray_8 text-[16px] cursor-pointer"
   >
-    <p>Services</p>
-    <p>Digital Items</p>
-    <p>Physical Products</p>
+    <Link href="/services">
+      <p>Services</p>
+    </Link>
+    <Link href="/products/digital">
+      <p>Digital Items</p>
+    </Link>
+    <Link href="/products/physical">
+      <p>Physical Products</p>
+    </Link>
     <div className="flex gap-x-[10px]">
       <Buttons />
     </div>
