e0b401b7f40fad2567d47b86aa56090c6c498f50
SeniorTech <tech.eng.me@gmail.com>
2024-02-11 20:04:24 -0500

    I fix create bug

--- hooks/useMatterMarketData.tsx
+++ hooks/useMatterMarketData.tsx
@@ -1,8 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import getProductsByType from "../lib/firebase/getProductsByType"
+import { serviceCategories, digitalCategories, physicalCategories } from "@/lib/consts"
 
 const useMatterMarketData = (type) => {
   const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
+
+  const allNav = {
+    label: "ALL",
+    value: "ALL",
+  }
+  const [selectedNav, setSelectedNav] = useState(allNav.value) as any
+
+  const categories = useMemo(() => {
+    if (type === "Service") return [allNav, ...serviceCategories]
+    if (type === "Digital") return [allNav, ...digitalCategories]
+    if (type === "Physical") return [allNav, ...physicalCategories]
+    return []
+  }, [type])
 
   useEffect(() => {
     const init = async () => {
@@ -10,12 +25,23 @@ const useMatterMarketData = (type) => {
 
       setProducts(response)
     }
-
     init()
   }, [type])
 
+  useEffect(() => {
+    if (products && selectedNav) {
+      const temp = products.filter((product) => product.productCategory === selectedNav)
+      setFilteredProducts(temp)
+    }
+  }, [selectedNav, products])
+
   return {
     products,
+    categories,
+    setSelectedNav,
+    selectedNav,
+    filteredProducts,
+    setFilteredProducts,
   }
 }
 
