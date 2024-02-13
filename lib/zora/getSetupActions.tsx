242f8daccd0fc22abb215bad0b431728b25d59fc
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-13 09:30:29 -0300

    I cleanup hard-coded variables.

--- lib/zora/getSetupActions.tsx
+++ lib/zora/getSetupActions.tsx
@@ -3,24 +3,29 @@ import { getCallSaleData } from "onchain-magic"
 import dropAbi from "../abi/abi-ERC1155Drop.json"
 
 const getSetupActions = (adminWallet, ipfsCid) => {
+  //   TODO: dummy variables need replaced before mainnet launch
+  const dummyUsdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+  const dummyNextTokenId = 1
+  const dummySaleStart = 0
+  const dummyPricePerToken = 100
+
   const adminPermissionArgs = [0, adminWallet, 2]
   const minterPermissionArgs = [0, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, 4]
   const iface = new Interface(dropAbi)
-
   const minterPermissionCall = iface.encodeFunctionData("addPermission", minterPermissionArgs)
   const adminPermissionCall = iface.encodeFunctionData("addPermission", adminPermissionArgs)
-  const usdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
-  const nextTokenId = 1
+  const openEdition = 0
+  const maxUint64 = "18446744073709551615"
   const data = getCallSaleData({
-    tokenId: nextTokenId,
-    saleStart: 0,
-    saleEnd: "18446744073709551615",
-    maxTokensPerAddress: 0,
-    pricePerToken: 100,
+    tokenId: dummyNextTokenId,
+    saleStart: dummySaleStart,
+    saleEnd: maxUint64,
+    maxTokensPerAddress: openEdition,
+    pricePerToken: dummyPricePerToken,
     fundsRecipient: adminWallet,
-    erc20Address: usdcAddress,
+    erc20Address: dummyUsdcAddress,
   })
-  const callSaleArgs = [nextTokenId, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, data]
+  const callSaleArgs = [dummyNextTokenId, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, data]
   const setupNewTokenArgs = [`ipfs://${ipfsCid}`, 0]
   const setupNewTokenCall = iface.encodeFunctionData("setupNewToken", setupNewTokenArgs)
   const callSaleCall = iface.encodeFunctionData("callSale", callSaleArgs)
