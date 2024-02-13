635e436c9eafc48f59bebde4b20c81e080857705
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-13 09:13:50 -0300

    I setup token with USDC minter.

--- hooks/useCreate1155Contract.tsx
+++ hooks/useCreate1155Contract.tsx
@@ -4,11 +4,12 @@ import abi from "../lib/abi/abi-Zora1155CreatorProxy.json"
 import dropAbi from "../lib/abi/abi-ERC1155Drop.json"
 import handleTxError from "../lib/handleTxError"
 import usePrivySendTransaction from "./usePrivySendTransaction"
-import { store } from "onchain-magic"
+import { getCallSaleData, store } from "onchain-magic"
 import getZora1155ProxyAddress from "../lib/zora/getZora1155ProxyAddress"
 import { CHAIN_ID } from "../lib/consts"
 import useConnectedWallet from "./useConnectedWallet"
 import getCreatedContractAddress from "../lib/getCreatedContractAddress"
+import { BigNumber } from "ethers"
 
 const useCreate1155Contract = () => {
   const { authenticated } = usePrivy()
@@ -21,15 +22,30 @@ const useCreate1155Contract = () => {
       const adminPermissionArgs = [0, connectedWallet, 2]
       const minterPermissionArgs = [0, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, 4]
       console.log("SWEETS minterPermissionArgs", minterPermissionArgs)
-      const minterPermissionCall = new Interface(dropAbi).encodeFunctionData(
-        "addPermission",
-        minterPermissionArgs,
-      )
-      const adminPermissionCall = new Interface(dropAbi).encodeFunctionData(
-        "addPermission",
-        adminPermissionArgs,
-      )
-      const setupActions = [adminPermissionCall, minterPermissionCall]
+      const iface = new Interface(dropAbi)
+      const minterPermissionCall = iface.encodeFunctionData("addPermission", minterPermissionArgs)
+      const adminPermissionCall = iface.encodeFunctionData("addPermission", adminPermissionArgs)
+      const usdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+      const nextTokenId = 1
+      const data = getCallSaleData({
+        tokenId: nextTokenId,
+        saleStart: 0,
+        saleEnd: "18446744073709551615",
+        maxTokensPerAddress: 0,
+        pricePerToken: 100,
+        fundsRecipient: connectedWallet,
+        erc20Address: usdcAddress,
+      })
+      const callSaleArgs = [nextTokenId, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, data]
+      const setupNewTokenArgs = [`ipfs://${ipfsCid}`, 0]
+      const setupNewTokenCall = iface.encodeFunctionData("setupNewToken", setupNewTokenArgs)
+      const callSaleCall = iface.encodeFunctionData("callSale", callSaleArgs)
+      const setupActions = [
+        adminPermissionCall,
+        minterPermissionCall,
+        setupNewTokenCall,
+        callSaleCall,
+      ]
 
       const args = [
         `ipfs://${ipfsCid}`,
