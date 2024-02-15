72bd347ab5137a0ef26e228d7beaffecce7718f1
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-15 11:16:00 -0300

    I fix placeholder on Payout section.

--- components/PayoutFundsModal/PayoutFundsModal.tsx
+++ components/PayoutFundsModal/PayoutFundsModal.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import Modal from "../../shared/Modal"
 
 const PayoutFundsModal = ({ isVisible, handleClose }) => {
-  const [payoutValue, setPayoutOutValue] = useState("")
+  const [coinbaseUsdcPayoutAddress, setCoinbaseUsdcPayoutAddress] = useState("")
 
   return (
     <Modal isVisible={isVisible} onClose={handleClose} containerClassName="w-full md:w-[375px]">
@@ -12,9 +12,9 @@ const PayoutFundsModal = ({ isVisible, handleClose }) => {
         flex flex-col items-center gap-y-[20px]"
       >
         <Input
-          value={payoutValue}
-          onChange={(e) => setPayoutOutValue(e.target.value)}
-          placeholder="Payout Amount"
+          value={coinbaseUsdcPayoutAddress}
+          onChange={(e) => setCoinbaseUsdcPayoutAddress(e.target.value)}
+          placeholder="Coinbase USDC (base) Address"
           className="!border-gray_6 !bg-white"
           id="payout_amount"
           name="payout_amount"
@@ -24,7 +24,7 @@ const PayoutFundsModal = ({ isVisible, handleClose }) => {
           className="border border-gray_2 rounded-full
                         py-[10px] px-[20px] flex items-center gap-x-[10px]"
         >
-          Payout Funds
+          Payout
         </button>
       </div>
     </Modal>
