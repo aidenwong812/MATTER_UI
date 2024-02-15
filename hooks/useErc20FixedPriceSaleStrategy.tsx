bc56cd1790e3a99e81ba87c6b0d76f0c96a628ca
Sweets Sweetman <sweetmantech@gmail.com>
2024-02-15 12:42:31 -0300

    I test successful creation / purchase on BASE mainnet.

--- hooks/useErc20FixedPriceSaleStrategy.tsx
+++ hooks/useErc20FixedPriceSaleStrategy.tsx
@@ -2,16 +2,22 @@ import { CHAIN_ID, MINTER_ADDRESS } from "@/lib/consts"
 import abi from "@/lib/abi/ERC20FixedPriceSaleStrategy.json"
 import { getEncodedMinterArgs } from "onchain-magic"
 import { BigNumber } from "ethers"
-import useConnectedWallet from "./useConnectedWallet"
-import usePrivySendTransaction from "./usePrivySendTransaction"
+import useConnectedWallet from "@/hooks/useConnectedWallet"
+import usePrivySendTransaction from "@/hooks/usePrivySendTransaction"
+import { IS_TESTNET } from "@/lib/consts"
 
 const useErc20FixedPriceSaleStrategy = () => {
   const { sendTransaction } = usePrivySendTransaction()
   const { connectedWallet } = useConnectedWallet()
 
   const requestMintBatchByPrivy = async () => {
+    if (!connectedWallet) return { error: "please connect wallet" }
     const to = MINTER_ADDRESS
-    const targets = ["0xC6DF65460CeD63c5505B5935eDE7D2c955e4CB6b"]
+    const targets = [
+      IS_TESTNET
+        ? "0xC6DF65460CeD63c5505B5935eDE7D2c955e4CB6b"
+        : "0xd0b7956771475f1f40901d2b6dbce507a629cc21",
+    ]
     const ids = [1]
     const quantities = [1]
     const unusedKey = 0
