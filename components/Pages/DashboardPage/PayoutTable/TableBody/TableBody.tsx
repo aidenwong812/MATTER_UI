2a1d219e74b999040928215bee058082315826e2
LuckyDev0331 <jameswilliam03031@gmail.com>
2024-02-21 23:27:44 +1000

    update the error for "isMobile"

--- components/Pages/DashboardPage/PayoutTable/TableBody/TableBody.tsx
+++ components/Pages/DashboardPage/PayoutTable/TableBody/TableBody.tsx
@@ -1,17 +1,15 @@
-import TableRow from "../TableRow"
 import { useWallets } from "@privy-io/react-auth"
+import TableRow from "../TableRow"
 
 const TableBody = (): JSX.Element => {
-
   const { wallets } = useWallets();
-  // const { approveWithPrivy, balance, minterAllowance } = useUsdc()
   return (
     <tbody>
       {wallets.map((wallet, index) => (
+        // eslint-disable-next-line react/no-array-index-key
         <TableRow key={index} wallet={wallet} />
       ))}
     </tbody>
   );
 };
-
 export default TableBody
--- components/Pages/DashboardPage/PayoutTable/TableRow/TableRow.tsx
+++ components/Pages/DashboardPage/PayoutTable/TableRow/TableRow.tsx
@@ -1,45 +1,53 @@
 import useIsMobile from "../../../../../hooks/useIsMobile"
 import truncateEthAddress from "../../../../../lib/truncatedEthAddress"
 
-
 const TableRow = (props) => {
   const isMobile = useIsMobile()
 
   const itemClasses = `md:px-[20px] md:py-[16px] text-gray_7 text-[12px] leading-[16px] p-[10px] border border-gray_1`
-  
-  const connectedWallet = props.wallet?.address
-  
-  const connectedAtTimestamp = props.wallet?.connectedAt
 
-  function formatDate(dateString: any, isMobile: boolean): string {
-    const date = new Date(dateString);
+  const connectedAtTimestamp = props?.wallet?.connectedAt
+
+  const amount = props?.wallet?.fund
+  
+  const connectedWallet = props?.wallet?.address  
+
+  function formatDate(dateString: any): string {
+    const date = new Date(dateString)
     const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hourCycle: 'h23', // Use PM instead of AM
-      timeZoneName: 'short', // Abbreviated Time Zone Name
-    };
-    return date.toLocaleString('en-US', options);
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hourCycle: "h23", // Use PM instead of AM
+      hour12: true,
+      timeZoneName: "short", // Abbreviated Time Zone Name
+    }
+    return date.toLocaleString("en-US", options)
   }
-  
-  const  connectedAt = formatDate(connectedAtTimestamp, isMobile);
 
-  const isConnected = props.wallet?.isConnected;
-  
+  const connectedAt = formatDate(connectedAtTimestamp)
+
+  const isConnected = props?.wallet?.isConnected() ?? false;
+  console.log("isConnected",isConnected)
+  const connectedDate = connectedAt.split(",")[0];
+
+  const connectedTime = connectedAt.split(",")[1].split(" ")[1];
+
+  const connectedTimezone = connectedAt.split(",")[1].split(" ")[2]
+
   return (
     <tr>
       <td className={itemClasses}>
         {isMobile ? (
           <>
-            {connectedAt}
+            {connectedDate}
             <br />
-            00:00:PM
+            {connectedTime}
             <br />
-            EST
+            {connectedTimezone}
           </>
         ) : (
           // "00/0011/0000 00:00:PM EST"
@@ -52,18 +60,15 @@ const TableRow = (props) => {
             0.000 <br /> USDT
           </>
         ) : (
-          "0.000 USDT"
+          // "0.000 USDT"
+          amount
         )}
       </td>
       <td className={`${itemClasses} !text-link`}>
-        {isMobile
-          ? truncateEthAddress(connectedWallet)
-          : connectedWallet}
+        {isMobile ? truncateEthAddress(connectedWallet) : connectedWallet}
       </td>
       <td className={itemClasses}>
-        <p className="text-[12px] leading-[16px]">
-          {isConnected ? "Successfully" : "failed"}
-        </p>
+        <p className="text-[12px] leading-[16px]">{ !isConnected ? "failed" : "Successfully"}</p>
       </td>
     </tr>
   )
